7de80c8f86316f5b58d6f36375914978208a2a8f
Ckopei <cameron@kopels.com>
2019-11-06 15:49:11 -0500

    Fixed all of the incorrect formatting from a bad conflict resolution on merge conflicts. Everything is now correct, two buttons per row.

--- client/src/components/Buttons/index.js
+++ client/src/components/Buttons/index.js
@@ -2,76 +2,78 @@ import React from "react";
 import "./style.css";
 //Uses the Auth methods to actually login with the LoginForm Component.
 class Buttons extends React.Component {
-  myfunction() {
-    console.log("clicked");
-  }
+    myfunction() {
+        console.log("clicked");
+    }
 
-  render() {
-    return (
-      <div className="container buttonContainer">
-        <div className="row mx-auto">
-          <div className="col-6 mt-5">
-            <img
-              className="biggerImg"
-              src="/buttons/light.svg"
-              alt="bicycle"
-              onClick={this.props.updateScore}
-            />
-            <h3>light</h3>
-          </div>
-          <div className="col-6 mt-5">
-            <img
-              className="biggerImg"
-              src="/buttons/car.svg"
-              alt="car"
-              onClick={this.props.updateScore}
-            />
-            <h3>Car</h3>
-          </div>
-        </div>
-        <div className="row mx-auto">
-          <div className="col-6 mt-5">
-            <img
-              className="biggerImg"
-              src="/buttons/world.svg"
-              alt="earth"
-              onClick={this.props.updateScore}
-            />
-            <h3>world</h3>
-          </div>
-        </div>
-        <div className="row mx-auto">
-          <div className="col-4 text-center mt-5">
-            <img
-              className="biggerImg"
-              src="/buttons/water.svg"
-              alt="flowers"
-              onClick={this.props.updateScore}
-            />
-            <h3>water</h3>
-          </div>
-          <div className="col-4 text-center mt-5">
-            <img
-              className="biggerImg"
-              src="/buttons/windmill.svg"
-              alt="house"
-              onClick={this.props.updateScore}
-            />
-            <h3>windmill</h3>
-          </div>
-          <div className="col-4 text-center mt-5">
-            <img
-              className="biggerImg"
-              src="/buttons/light.svg"
-              alt="leaf"
-              onClick={this.props.updateScore}
-            />
-            <h3>light</h3>
-            </div>
-          </div>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container buttonContainer">
+                <div className="row mx-auto">
+                    <div className="col-6 text-center mt-5">
+                        <img
+                            className="biggerImg"
+                            src="/buttons/light.svg"
+                            alt="bicycle"
+                            onClick={this.props.updateScore}
+                        />
+                        <h3>light</h3>
+                    </div>
+                    <div className="col-6 text-center mt-5">
+                        <img
+                            className="biggerImg"
+                            src="/buttons/car.svg"
+                            alt="car"
+                            onClick={this.props.updateScore}
+                        />
+                        <h3>Car</h3>
+                    </div>
+                </div>
+                <div className="row mx-auto">
+                    <div className="col-6 text-center mt-5">
+                        <img
+                            className="biggerImg"
+                            src="/buttons/world.svg"
+                            alt="earth"
+                            onClick={this.props.updateScore}
+                        />
+                        <h3>world</h3>
+                    </div>
+
+
+                    <div className="col-6 text-center mt-5">
+                        <img
+                            className="biggerImg"
+                            src="/buttons/water.svg"
+                            alt="flowers"
+                            onClick={this.props.updateScore}
+                        />
+                        <h3>water</h3>
+                    </div>
+                </div>
+                <div className="row mx-auto">
+                    <div className="col-6 text-center mt-5">
+                        <img
+                            className="biggerImg"
+                            src="/buttons/windmill.svg"
+                            alt="house"
+                            onClick={this.props.updateScore}
+                        />
+                        <h3>windmill</h3>
+                    </div>
+                    <div className="col-6 text-center mt-5">
+                        <img
+                            className="biggerImg"
+                            src="/buttons/light.svg"
+                            alt="leaf"
+                            onClick={this.props.updateScore}
+                        />
+                        <h3>light</h3>
+                    </div>
+                </div>
+            </div >
+        );
+    }
 }
 
 export default Buttons;
