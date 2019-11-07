6cfbff0c3c005a476290fea0344734f7202cad7d
Ckopei <cameron@kopels.com>
2019-11-07 12:04:28 -0500

    changed app.use route

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(flash());
 
 /* Serve up static assets (usually on heroku) */
 if (process.env.NODE_ENV === "production") {
-  app.use(passport.session()); app.use(express.static(path.join(__dirname, '../build')));
+  app.use(passport.session()); app.use(express.static(path.join(__dirname, './client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
