69a6c42d8b5083eb68d1724c73f04846b6dabf33
Ckopei <cameron@kopels.com>
2019-11-07 11:26:02 -0500

    Testing a heroku deployment.

---
+++ client/build/precache-manifest.5a964c6b16fb6bf02f5751b6609660cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69918061932c2e7188bdc0e3136fe743",
+    "url": "/static/media/background.69918061.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbaf383af38620ba2340",
+    "url": "/static/js/main.dbaf383a.chunk.js"
+  },
+  {
+    "revision": "9f5217f8bfa58fd54bcd",
+    "url": "/static/js/1.9f5217f8.chunk.js"
+  },
+  {
+    "revision": "dbaf383af38620ba2340",
+    "url": "/static/css/main.e764a7ef.chunk.css"
+  },
+  {
+    "revision": "9721c1164fac3b68b694ff57e61f04ad",
+    "url": "/index.html"
+  }
+];
