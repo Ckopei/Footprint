367e092ea4f446ed6bfe1decaaae085d7b814d70
Mal <malrsteffes@gmail.com>
2019-11-07 21:11:36 -0500

    navbar on the about page is fixed. added a miniabout component to the main page. let me know if you prefer the minimal look - we can definitely figure something else out to stil give unregistered users access

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "../Grid";
 import Title from "../Title/title";
-import Nav from "../Nav/index";
+import LogOutNav from "../LogOutNav/LogOutNav";
 import "./style.css";
 import { Link } from "react-router-dom";
 
@@ -9,7 +9,7 @@ class About extends React.Component {
   render() {
     return (
       <div>
-        <Nav />
+        <LogOutNav />
         <Container fluid>
           <div className="container">
             <Title />
@@ -51,7 +51,7 @@ class About extends React.Component {
                   <img
                     src="https://kopel.dev/assets/img/itsAMeMario.jpg"
                     className="rounded-circle z-depth-1"
-                    alt="Mallory_Steffes"
+                    alt="Cameron_Kopel"
                   />
 
                   <h5 className="aboutName">Cameron Kopel</h5>
