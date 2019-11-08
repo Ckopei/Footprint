9d02bc865395118d13aa09782225986eca040fe7
Ckopei <cameron@kopels.com>
2019-11-07 20:24:44 -0500

    Cleaned up commented out code, needless comments, and variables that were declared but never used.

---
+++ client/build/precache-manifest.6719d317a7ce08f4e7131214425be7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69918061932c2e7188bdc0e3136fe743",
+    "url": "/static/media/background.69918061.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c3e4851a8b486363a2d",
+    "url": "/static/js/main.6c3e4851.chunk.js"
+  },
+  {
+    "revision": "ab84fd673b8bf3a3d15e",
+    "url": "/static/js/1.ab84fd67.chunk.js"
+  },
+  {
+    "revision": "6c3e4851a8b486363a2d",
+    "url": "/static/css/main.14885fbf.chunk.css"
+  },
+  {
+    "revision": "ec588d87f3ba5aa06b800dcf31877b32",
+    "url": "/index.html"
+  }
+];
