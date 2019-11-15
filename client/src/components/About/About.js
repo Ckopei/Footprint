238a62fbb11235157d1fa22ec0bf70aee2a92790
Mal <malrsteffes@gmail.com>
2019-11-14 19:20:38 -0500

    About page has been redone, please check it out and oh yes it's mobile responsive

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,89 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import { Spring } from "react-spring/renderprops";
-
-import { Container } from "../Grid";
+import { Container, Row, Col } from "../Grid";
 import Title from "../Title/title";
 import LogOutNav from "../LogOutNav/LogOutNav";
+import Card from "../Card";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-class About extends React.Component {
+class NewAbout extends React.Component {
   render() {
     return (
       <div>
         <LogOutNav />
-        <Container fluid>
-          <div className="container">
-            <Title />
+        <Container>
+          <Title />
+          <br></br>
+          <div className="container about">
+            <div className="container text-center aboutApp">
+              <Row>
+                <Col size="md-12" className="text-center">
+                  <h3 className="text-center">About the App</h3>
+                  <br></br>
+                  <p className="lead">
+                    <span className="strong">Footprint</span> is an app designed
+                    to make staying environmentally conscious a bit more fun.
+                    You can create an account, check out our tips and tricks,
+                    and start tracking what you're doing to help the world you
+                    live in. <br></br>
+                    <Link to="/register">Sign up </Link>to get started!
+                  </p>
+                </Col>
+              </Row>
+            </div>
+            <div className="creatorContainer">
+              <Row>
+                <Col size="md-12">
+                  <h3 className="text-center">About the Creators</h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col size="lg-6 md-12" className="text-center">
+                  <Card
+                    id="malCard"
+                    title="Mallory Steffes"
+                    avatar="https://avatars3.githubusercontent.com/u/52713263?s=460&v=4"
+                    bio="I’m a full-stack developer, currently based in
+                  Orlando. I de-stress by playing board games,
+                  weightlifting, and indulging a bad online shopping
+                  habit."
+                    linkedIn="https://www.linkedin.com/in/mallorysteffes/"
+                    github="https://github.com/malloryrsteffes"
+                    mail="mailto:malrsteffes@gmail.com"
+                  ></Card>
+                </Col>
+                <Col size="lg-6 md-12" className="text-center">
+                  <Card
+                    id="camCard"
+                    title="Cameron Kopel"
+                    avatar="https://kopel.dev/assets/img/itsAMeMario.jpg"
+                    bio="I’m a full-stack developer living in Orlando. I love
+                    traveling, and impulsively buy and collect guitars."
+                    linkedIn="https://www.linkedin.com/in/cameronkopel/"
+                    github="https://github.com/Ckopei"
+                    mail="mailto:cameron@kopels.com"
+                  ></Card>
+                </Col>
+              </Row>
+            </div>
           </div>
-          <Spring
-            from={{ opacity: 0, marginTop: -1000 }}
-            to={{ opacity: 1, marginTop: 0 }}
-            config={{ duration: 800 }}
-          >
-            {props => (
-              <div style={props}>
-                <div className="aboutContainer">
-                  <div className="container text-center aboutApp">
-                    <h3 className="jumbotron-heading">About the App</h3>
-                    <p className="lead">
-                      <span className="strong">Footprint</span> is an app
-                      designed to make staying environmentally conscious a bit
-                      more fun. You can create an account, check out our tips
-                      and tricks, and start tracking what you're doing to help
-                      the world you live in. <br></br>
-                      <Link to="/register">Sign up </Link>to get started!
-                    </p>
-                  </div>
-                  <div className="container text-center creatorContainer">
-                    <div className="row">
-                      <div className="col-lg-12 mb-lg-0 mb-5 text-center">
-                        <h2 className="jumbotron-heading">Behind the App</h2>
-                      </div>
-                      <div className="col-lg-4 offset-1 col-sm-12 avatar text-center">
-                        <img
-                          src="https://avatars3.githubusercontent.com/u/52713263?s=460&v=4"
-                          className="rounded-circle z-depth-1"
-                          alt="Mallory_Steffes"
-                        />
-
-                        <h5 className="aboutName">Mallory Steffes</h5>
-                        <p className="aboutPosition">
-                          <strong>Front-End Developer</strong>
-                        </p>
-                        <p className="aboutText">
-                          I’m a full-stack developer, currently based in
-                          Orlando. I de-stress by playing board games,
-                          weightlifting, and indulging a bad online shopping
-                          habit.
-                        </p>
-                      </div>
-                      <div className="col-lg-4 offset-2 col-sm-12 avatar text-center">
-                        <img
-                          src="https://kopel.dev/assets/img/itsAMeMario.jpg"
-                          className="rounded-circle z-depth-1"
-                          alt="Cameron_Kopel"
-                        />
-
-                        <h5 className="aboutName">Cameron Kopel</h5>
-                        <p className="aboutPosition">
-                          <strong>Back-End Developer</strong>
-                        </p>
-                        <p className="aboutText">
-                          I’m a full-stack developer living in Orlando. I love
-                          traveling, and impulsively buy and collect guitars.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )}
-          </Spring>
         </Container>
       </div>
     );
   }
 }
 
-export default About;
+export default NewAbout;
--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 
-function Card({ icon, title, children }) {
+function Card({ id, avatar, title, bio, linkedIn, github, mail }) {
   return (
-    <div className="card mt-4">
-      <div className="card-header">
-        <h3>
+    <div className="card" id={id}>
+      <div className="card-body text-center">
+        <img className="avatar rounded-circle"src={avatar} alt="portfolio"></img>
+        <h3 className="card-title">
           <strong>
-            <i className={`fa fa-${icon}`} aria-hidden="true" /> {title}
+             {title}
           </strong>
         </h3>
+        <hr></hr>
+        <div className="card-text">{bio}</div>
+        <div className="icon-block">
+          <a href={linkedIn}><i className="fa fa-linkedin"></i></a>
+          <a href={github}><i className="fa fa-github"></i></a>
+          <a href={mail}><i className="fa fa-envelope"></i></a>
+
+        </div>
       </div>
-      <div className="card-body">{children}</div>
+      
     </div>
   );
 }
