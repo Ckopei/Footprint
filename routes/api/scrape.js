ee0c47fdf5890a421ba2c0edfcc2a2649ba72623
Ckopei <cameron@kopels.com>
2019-11-17 20:38:09 -0500

    Added filter on scrape router to filter any reddit relative links.

--- routes/api/scrape.js
+++ routes/api/scrape.js
@@ -19,24 +19,29 @@ router.route("/").get(function (req, res) {
       // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
+      if ($(this).attr("href").includes("/r/environment/")) {
+        return;
+      } else {
+         // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        //   .children("a")
-        .text();
-      result.link = $(this)
-        //   .children("a")
-        .attr("href");
+      //   .children("a")
+      .text();
+    result.link = $(this)
+      //   .children("a")
+      .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
-      db.create(result)
-        .then(function (dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function (err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
+    // Create a new Article using the `result` object built from scraping
+    db.create(result)
+      .then(function (dbArticle) {
+        // View the added result in the console
+        console.log(dbArticle);
+      })
+      .catch(function (err) {
+        // If an error occurred, log it
+        console.log(err);
+      });
+      }
+     
     });
 
     // Send a message to the client
